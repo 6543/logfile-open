3053f3a5082919770e9082679749c6e9f8d8ac80
6543 <6543@obermui.de>
2023-08-05 04:10:17 +0200

    create wrapper

--- open.go
+++ open.go
@@ -3,8 +3,54 @@
 
 package logfile
 
-import "os"
+import (
+	"fmt"
+	"io"
+	"os"
+	"sync"
+)
 
-func OpenFile(name string, flag int, perm os.FileMode) (*os.File, error) {
-	return os.OpenFile(name, flag, perm)
+type wrapper struct {
+	fileName string
+	origFile *os.File
+	lock     sync.RWMutex
+	err      error
+}
+
+func (w *wrapper) Close() error {
+	if w.err != nil {
+		return w.err
+	}
+	w.lock.Lock() // lock indefinite
+	w.err = fmt.Errorf("writer got closed")
+	return w.origFile.Close()
+}
+
+func (w *wrapper) Write(p []byte) (n int, err error) {
+	if w.err != nil {
+		return 0, w.err
+	}
+	w.lock.Lock()
+	defer w.lock.Unlock()
+	return w.origFile.Write(p)
+}
+
+func (w *wrapper) Read(p []byte) (n int, err error) {
+	if w.err != nil {
+		return 0, w.err
+	}
+	w.lock.RLock()
+	defer w.lock.RUnlock()
+	return w.origFile.Read(p)
+}
+
+func OpenFile(name string, flag int, perm os.FileMode) (io.ReadWriteCloser, error) {
+	file, err := os.OpenFile(name, flag, perm)
+	if err != nil {
+		return nil, err
+	}
+	return &wrapper{
+		fileName: name,
+		origFile: file,
+	}, nil
 }
