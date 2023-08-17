8606cce403f7e6face6f582b5dfcb089e0520349
6543 <6543@obermui.de>
2023-08-17 22:38:27 +0200

    windows respect context too

--- open_windows.go
+++ open_windows.go
@@ -17,6 +17,68 @@ func OpenFile(name string, perm os.FileMode) (io.ReadWriteCloser, error) {
 	return os.OpenFile(name, os.O_CREATE|os.O_RDWR|os.O_APPEND, perm)
 }
 
-func OpenFileWithContext(_ context.Context, name string, perm os.FileMode) (io.ReadWriteCloser, error) {
-	return OpenFile(name, perm)
+type wrapper struct {
+	ctx            context.Context
+	ctxCloser      func()
+	origFile       io.ReadWriteCloser
+}
+
+func (w *wrapper) Close() error {
+	err := w.origFile.Close()
+	w.ctxCloser()
+	w.ctxCloser = func() {}
+	return err
+}
+
+func (w *wrapper) Write(p []byte) (n int, err error) {
+	return w.origFile.Write(p)
+}
+
+func (w *wrapper) Read(p []byte) (n int, err error) {
+	return w.origFile.Read(p)
+}
+
+func (w *wrapper) contextListener() {
+	for {
+		select {
+		case <-w.ctx.Done():
+			origFile.Close()
+			return
+	}
+}
+
+func OpenFileWithContext(ctx context.Context, name string, perm os.FileMode) (io.ReadWriteCloser, error) {
+	file, err := OpenFile(name, perm)
+	if err != nil {
+		return nil, err
+	}
+
+	newCtx, ctxCancel := context.WithCancel(ctx)
+
+	rwc := &wrapper{
+		ctx:            newCtx,
+		ctxCloser:      ctxCancel,
+		origFile:       file,
+	}
+
+	go rwc.contextListener()
+
+	return rwc, nil
+}
+
+
+func OpenFileWithContext(ctx context.Context, name string, perm os.FileMode) (io.ReadWriteCloser, error) {
+	file, err := OpenFile(name, perm)
+
+	go func() {
+		file := file
+		for {
+			select {
+			case <-w.ctx.Done():
+				w.close()
+				return
+		}
+	}()
+
+	return file, err
 }
